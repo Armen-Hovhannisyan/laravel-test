5a0c4bc62a667fc4827eefbfdb37dac3a9b4b552
Armen-Hovhannisyan <armen070789@gmail.com>
2018-08-25 00:44:12 +0500

    finish

---
+++ resources/views/news_list.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>List of News Section</h4>
+                <p>Find the updates on Latest News in this area</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="row mb-2">
+                    <div class="col-md-12">
+                        <div class="card bg-warning">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="http://www.3forty.media/cannix/wp-content/uploads/2018/03/clem-onojeghuo-127166-unsplash-1-500x333.jpg ">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="news-title">
+                                            <a href="#"><h5>Uneasy no settle whence nature narrow in afraid</h5></a>
+                                        </div>
+                                        <div class="news-cats">
+                                            <ul class="list-unstyled list-inline mb-1">
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>Advertisment</small>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>Author</small>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>4th July 2018</small>
+                                                    </a>
+                                                </li>
+
+
+                                            </ul>
+                                        </div>
+                                        <div class="news-content">
+                                            <p>Particular unaffected projection sentiments no my. Music marry as at
+                                                cause party worth weeks. S... </p>
+                                        </div>
+                                        <div class="news-buttons">
+                                            <button type="button" class="btn btn-outline-danger btn-sm">Read More
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <img src="http://www.3forty.media/cannix/wp-content/uploads/2018/03/clem-onojeghuo-127166-unsplash-1-500x333.jpg ">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="news-title">
+                                            <a href="#"><h5>Uneasy no settle whence nature narrow in afraid</h5></a>
+                                        </div>
+                                        <div class="news-cats">
+                                            <ul class="list-unstyled list-inline mb-1">
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>Advertisment</small>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>Author</small>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <i class="fa fa-folder-o text-danger"></i>
+                                                    <a href="#">
+                                                        <small>4th July 2018</small>
+                                                    </a>
+                                                </li>
+
+
+                                            </ul>
+                                        </div>
+                                        <div class="news-content">
+                                            <p>Particular unaffected projection sentiments no my. Music marry as at
+                                                cause party worth weeks. S... </p>
+                                        </div>
+                                        <div class="news-buttons">
+                                            <button type="button" class="btn btn-outline-danger btn-sm">Read More
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+@endsection
